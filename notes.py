9a37d279fd4248cb9d5199331c82092e0ab2cc3b
IvelinPenchev <ivelin.penchev.95@gmail.com>
2022-03-13 19:25:05 +0100

    setup

---
+++ notes.py
@@ -0,0 +1,12 @@
+# To create a new python environment do the following: 
+#   python3 -m venv .venv
+# To activate new environment type:
+#   source .venv/bin/activate   (MAC)
+#   cd .venv/Scripts (WINDOWS - remember running in cmd and NOT powershell)
+#   activate (WINDOWS - remember running in cmd and NOT powershell)
+# To export packages used for a project use 
+#  pip3 freeze > requirements.txt
+# To add a specific packages to the environment: 
+#  pip3 install -r requirements.txt
+
+
