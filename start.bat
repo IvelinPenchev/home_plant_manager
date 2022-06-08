3d203d7a541280bc605dacbb746851aeb670afa9
IvelinPenchev <ivelin.penchev.95@gmail.com>
2022-06-08 15:42:15 +0200

    credentials removal

---
+++ start.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+START cmd /k "cd /d catalogue_server\.venv\Scripts & activate & cd .. & cd .. & python app.py"
+timeout 0.5
+START cmd /k "cd /d data_base\.venv\Scripts & activate & cd .. & cd .. & python app.py"
+timeout 0.5
+START cmd /k "cd /d telegram_bot\.venv\Scripts & activate & cd .. & cd .. & python main.py"
